354157c1a52b3d91b7580b666d667563abd53084
avislash <avi@algowatcher.net>
2023-06-06 09:31:13 -0400

    Add handling for MAYC Metadata

--- cartel/metadata/fetcher.go
+++ cartel/metadata/fetcher.go
@@ -8,6 +8,14 @@ type HoundMetadataFetcher struct {
 	metadata.Fetcher[HoundMetadata]
 }
 
+type MAYCMetadataFetcher struct {
+	metadata.Fetcher[MAYCMetadata]
+}
+
 func NewHoundMetadataFetcher(baseURL string) *HoundMetadataFetcher {
 	return &HoundMetadataFetcher{metadata.NewJSONMetadataFetcher[HoundMetadata](baseURL)}
+}
+
+func NewMAYCMetadataFetcher(baseURL string) *MAYCMetadataFetcher {
+	return &MAYCMetadataFetcher{metadata.NewJSONMetadataFetcher[MAYCMetadata](baseURL)}
 }
--- cartel/metadata/metadata.go
+++ cartel/metadata/metadata.go
@@ -59,3 +59,68 @@ func (hmd *HoundMetadata) UnmarshalJSON(data []byte) error {
 
 	return nil
 }
+
+type MAYCMetadata struct {
+	Name       string
+	Image      string
+	Background string
+	Mouth      string
+	Clothes    string
+	Earring    string
+	Eyes       string
+	Fur        string
+	Hat        string
+}
+
+func (mm *MAYCMetadata) UnmarshalJSON(data []byte) error {
+	s := struct {
+		Name       string            `json:"name"`
+		Image      string            `json:"image"`
+		Attributes []json.RawMessage `json:"attributes"`
+	}{}
+
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	for _, _attribute := range s.Attributes {
+		attribute := make(map[string]string)
+		err := json.Unmarshal(_attribute, &attribute)
+		if err != nil {
+			return err
+		}
+
+		if attribute["trait_type"] == "Background" {
+			mm.Background = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+
+		if attribute["trait_type"] == "Mouth" {
+			mm.Mouth = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+
+		if attribute["trait_type"] == "Clothes" {
+			mm.Clothes = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+
+		if attribute["trait_type"] == "Earring" {
+			mm.Earring = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+
+		if attribute["trait_type"] == "Eyes" {
+			mm.Eyes = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+
+		if attribute["trait_type"] == "Fur" {
+			mm.Fur = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+
+		if attribute["trait_type"] == "Hat" {
+			mm.Hat = strings.TrimSpace(strings.ToLower(attribute["value"]))
+		}
+	}
+	mm.Name = s.Name
+	mm.Image = s.Image
+
+	return nil
+}
